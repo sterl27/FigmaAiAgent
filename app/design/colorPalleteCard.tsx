e51bb7a390a9dd240bd4139cde9c84b0bdbd56b3
Sterl <sterl27@gmail.com>
2025-12-02 22:46:03 -0600

    fix: Add missing avatar component and fix syntax error

--- app/design/colorPalleteCard.tsx
+++ app/design/colorPalleteCard.tsx
@@ -65,36 +65,4 @@ export default function ColorPaletteCard({ color }: { color: { hex: string, usag
             </div>
         </div>
     )
-}       >
-            <div className="bg-white w-full p-4 rounded-tl-lg rounded-tr-lg border-t border-l border-r border-gray-200">
-                <div className="relative inline-block mb-2">
-                    <TooltipProvider>
-                        <Tooltip>
-                            <TooltipTrigger asChild>
-                                <button
-                                    type="button"
-                                    className="bg-gray-100 text-black font-mono font-semibold py-2 px-4 rounded border-0 cursor-pointer relative transition-all duration-200 ease-in-out text-base hover:bg-[#e5e7eb] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                                    data-color={color.hex}
-                                    aria-label={`Copy color code ${color.hex}`}
-                                    onClick={() => copyToClipboard(color.hex)}
-                                >
-                                    {color.hex}
-                                    {/* <span className="tooltip">Click to copy</span> */}
-                                </button>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>Click to copy</p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                    {copied && (
-                        <div className="absolute bottom-full left-1/2 -translate-x-1/2 bg-blue-900 text-white text-xs py-1 px-2 rounded whitespace-nowrap mb-2 font-sans font-normal">
-                            Copied!
-                        </div>
-                    )}
-                </div>
-                <div className="m-0 text-black text-sm leading-normal">{color.usage}</div>
-            </div>
-        </div>
-    )
 }
